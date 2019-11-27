c0be2395c44cb6a07c56d9e37899a0d9ac970063
Yannick Sagaert <yannick.sagaert@student.howest.be>
2019-11-27 15:27:25 +0100

    Add EntranceFee

---
+++ Zoo.lib/Zoo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.lib
+{
+    class Zoo
+    {
+
+        public decimal EntranceFee { get; set; } = 50.0M;
+
+        public Zoo(decimal entranceFee)
+        {
+            EntranceFee = entranceFee;
+        }
+
+    }
+}
