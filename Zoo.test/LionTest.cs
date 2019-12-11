fee2829c2b110af9f6b217dd21b3e68aae92ce9b
Yannick Sagaert <yannick.sagaert@student.howest.be>
2019-12-11 14:36:29 +0100

    Add unit test for lion

---
+++ Zoo.test/LionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Zoo.lib;
+
+namespace Zoo.test
+{
+    public class LionTest
+    {
+        [Fact]
+        public void Declare_LionWithNameOnly_HasCorrectName()
+        {
+            // Arrange
+            Lion lion;
+
+            // Act
+            var name = "A";
+            lion =  new Lion(name);
+            var actual = lion.Name;
+            var expected = (name);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
