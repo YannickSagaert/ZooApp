e1339fa03110873a641e02e44f8d02f7e5980c03
Yannick Sagaert <yannick.sagaert@student.howest.be>
2019-12-11 14:12:03 +0100

    Add age to lions

--- Zoo.lib/Lion.cs
+++ Zoo.lib/Lion.cs
@@ -13,10 +13,23 @@ namespace Zoo.lib
         public Sex Sex { get; set; }
         public string Name { get; set; }
 
-        public Lion(string name, Sex sex)
+        public int Age { get; set; }
+
+        public Lion(string name): this(name,Sex.F)
+        {
+
+        }
+
+        public Lion(string name, Sex sex): this(name,sex,0)
+        {
+           
+        }
+
+        public Lion(string name, Sex sex, int age)
         {
             Name = name;
             Sex = sex;
+            Age = age;
         }
     }
 }
