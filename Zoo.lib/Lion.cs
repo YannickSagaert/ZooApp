053898286602be1f53bbf08abd796c1fe11640ba
Yannick Sagaert <yannick.sagaert@student.howest.be>
2019-12-11 13:32:39 +0100

    add description for lion class

--- Zoo.lib/Lion.cs
+++ Zoo.lib/Lion.cs
@@ -5,6 +5,9 @@ using System.Text;
 namespace Zoo.lib
 {
     public enum Sex { F, M };
+    /// <summary>
+    /// This class describes léoné veri dangeros enimel
+    /// </summary>
     public class Lion
     {
         public Sex Sex { get; set; }
